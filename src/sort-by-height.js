8aa5cfecb4c2c1fa890672b97f869332eef8e6f0
Elizaveta Biryukova <birliza@mail.ru>
2023-04-17 12:39:08 +0200

    add tasks

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  let arrNew = arr.filter(el => el !== -1).sort(function(a,b){ return a-b});
+
+
+  for(let i=0, j=0; i<arr.length; i++){
+        if(arr[i]!==-1){
+          arr[i] = arrNew[j];
+          j++;
+        }
+      }
+      return arr;
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
 /**
  * Create transformed array based on the control sequences that original
@@ -13,9 +15,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  let newArr = arr.slice(0);
+
+  for (let i = 0; i < newArr.length; i++) {
+    if (newArr[i] === '--discard-next') {
+      newArr.splice(i, 2, 'none', 'none');
+    } else if (newArr[i] === '--discard-prev') {
+      if (i === 0) {
+        newArr.splice(i, 1, 'none');
+      } else {
+        newArr.splice(i - 1, 2, 'none', 'none');
+      }
+    } else if (newArr[i] === '--double-next') {
+      if (i === newArr.length - 1) {
+        newArr.splice(i, 1);
+      } else {
+        newArr.splice(i, 1, newArr[i + 1]);
+      }
+    } else if (newArr[i] === '--double-prev') {
+      if (i === 0) {
+        newArr.splice(i, 1);
+      } else {
+        newArr.splice(i, 1, newArr[i - 1]);
+      }
+    }
+    // return newArr.filter(el => el !== 'none');
+  }
+  return newArr.filter(el => el !== 'none');
+
 }
 
 module.exports = {
